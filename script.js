e4cc3abc8c858744f14ff5e194b7226014520e76
thisiskavi <kavitha4097@gmail.com>
2023-11-03 18:04:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+var API = "https://restcountries.com/v3.1/all";
+
+
+var fet = fetch(API)
+  .then((response) => response.json())
+  .then((data) => {
+    
+    data.map((value) => {
+      var spreadOperator = {
+        ...value,
+        name: value.name.common,
+        flag: value.flags.png,
+        code: value.cioc,
+        capital: value.capital,
+        region: value.region,
+        latitude: value.latlng[0],
+        longitude: value.latlng[1]
+
+      };
+      createcountry(spreadOperator);
+      
+      // console.log(value)
+    })
+  })
+  
+function createcountry({ name, flag, code, capital, region, latitude, longitude }) {
+   
+  document.body.innerHTML += 
+  ` <div class="container">
+    <div class="card"  >
+    <h1 id="title" class="text-center">${name}</h1>
+    <img src="${flag}" class="flag" alt="${name}'Flag image">
+ 
+  <div class="card-body car" >
+  <p><span>Captial :</span> ${capital}</p>
+  <p><span>Region :</span> ${region}</p>
+  <p><span>Country Code :</span>${code}</p>
+  <a href="#" class="btn btn-primary" onclick=(block(${latitude},${longitude},${name})) >Weather Info</a>
+ <div id=${name}>   </div>
+  
+ 
+  </div>
+</div>
+</div>
+`
+}
+
+function block(lat,lng,name){
+
+  var WAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=d7369de7d761f134d96aa3e8f332f53f`
+   
+  console.log(name)
+ fetch(WAPI)
+ .then((response) => response.json())
+ .then((data)=> {
+
+     alert(`
+               For ${name.id}  
+     Current Humidity is ${data.main.humidity}
+     Current Pressure is ${data.main.pressure}
+     Current Temperature is ${data.main.temp}`)
+    
+    })
+}
+  
+document.addEventListener("click",(event) => event.preventDefault())
